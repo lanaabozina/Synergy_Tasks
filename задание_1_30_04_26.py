814d3358a1c33aa9afd7626f921d0ea8a9d3a83c
lanaabozina <laniiicat@yandex.ru>
2026-04-30 10:54:19 +0300

    Add files via upload

---
+++ задание_1_30_04_26.py
@@ -0,0 +1,13 @@
+# Ввод сторон прямоугольника
+a = float(input("Введите длину стороны A: "))
+b = float(input("Введите длину стороны B: "))
+
+# Расчёты
+area = a * b
+perimeter = 2 * (a + b)
+
+# Вывод результата
+print("Площадь прямоугольника:", area)
+print("Периметр прямоугольника:", perimeter)
+
+# Ссылка на GitHub: https://github.com/lanaabozina/Synergy_Tasks
